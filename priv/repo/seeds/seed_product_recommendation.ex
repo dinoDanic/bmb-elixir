330805397692d3508472cc725b2dca7d85d40927
dinoDanic <dino.danic@gmail.com>
2023-06-06 20:48:57 +0200

    pr

---
+++ priv/repo/seeds/seed_product_recommendation.ex
@@ -0,0 +1,18 @@
+alias Ecto.Repo
+alias Bmb.ProductRecommendations
+alias Bmb.Repo
+
+product_recomendation = [
+  %{product_id: 1, recommended_product_id: 62 },
+  %{product_id: 1, recommended_product_id: 47 },
+  %{product_id: 1, recommended_product_id: 58 },
+  %{product_id: 1, recommended_product_id: 62 },
+]
+
+
+Repo.transaction(fn ->
+  Enum.each(product_recomendation, fn %{product_id: product_id, recommended_product_id: recommended_product_id} ->
+    product_recomendations = %ProductRecommendations{product_id: product_id, recommended_product_id: recommended_product_id}
+    Repo.insert!(product_recomendations)
+  end)
+end)
