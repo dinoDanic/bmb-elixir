14e01bab88a2659bf23ee5345069cb6c81c5800f
dinodanic <dino.danic@gmail.com>
2023-07-20 21:48:47 +0200

    pool revert

--- config/dev.exs
+++ config/dev.exs
@@ -9,7 +9,7 @@ config :bmb, Bmb.Repo,
   database: "railway",
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
-  pool_size: 30
+  pool_size: 10
 
 # For development, we disable any cache and enable
 # debugging and code reloading.
