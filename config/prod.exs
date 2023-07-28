86f3a7141e926f2f951457c7c4938a05ea7d1b77
Dino Đanić <dino.danic@gmail.com>
2023-07-28 15:21:52 +0200

    docker

--- config/prod.exs
+++ config/prod.exs
@@ -7,6 +7,16 @@ import Config
 # Configures Swoosh API Client
 config :swoosh, api_client: Swoosh.ApiClient.Finch, finch_name: Bmb.Finch
 
+config :bmb, Bmb.Repo,
+  username: System.get_env("PGUSER"),
+  password: System.get_env("PGPASSWORD"),
+  hostname: System.get_env("PGHOST"),
+  database: System.get_env("PGDATABASE"),
+  port: System.get_env("PGPORT"),
+  stacktrace: true,
+  show_sensitive_data_on_connection_error: true,
+  pool_size: 10
+
 # CORS
 config :cors_plug,
   origin: [
